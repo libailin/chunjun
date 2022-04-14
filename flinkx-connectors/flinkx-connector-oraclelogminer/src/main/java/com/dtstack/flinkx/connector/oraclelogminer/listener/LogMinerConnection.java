907bd4050b92d36c82b055f9bb87d1b72b516e3f
libailin <bailin_li@163.com>
2022-04-14 15:45:47 +0800

    Revert "[hotfix-53323][logminer]Data is lost due to log group switchover during online log loading"
    This reverts commit 99102626092e67ce7edd7361d1b5fd6187e53787.

--- flinkx-connectors/flinkx-connector-oraclelogminer/src/main/java/com/dtstack/flinkx/connector/oraclelogminer/listener/LogMinerConnection.java
+++ flinkx-connectors/flinkx-connector-oraclelogminer/src/main/java/com/dtstack/flinkx/connector/oraclelogminer/listener/LogMinerConnection.java
@@ -37,7 +37,6 @@ import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -511,7 +510,7 @@ public class LogMinerConnection {
     }
 
     /** 根据leftScn 以及加载的日志大小限制 获取可加载的scn范围 以及此范围对应的日志文件 */
-    protected Pair<BigInteger, Boolean> getEndScn(BigInteger startScn, List<LogFile> logFiles)
+    protected BigInteger getEndScn(BigInteger startScn, List<LogFile> logFiles)
             throws SQLException {
 
         List<LogFile> logFileLists = new ArrayList<>();
@@ -558,7 +557,6 @@ public class LogMinerConnection {
                                         .collect(Collectors.toList())));
 
         BigInteger endScn = startScn;
-        Boolean loadRedoLog = false;
 
         long fileSize = 0L;
         Collection<List<LogFile>> values = map.values();
@@ -600,16 +598,10 @@ public class LogMinerConnection {
             // 解决logminer偶尔丢失数据问题，读取online日志的时候，需要将rightScn置为当前SCN
             endScn = getCurrentScn();
             logFiles = logFileLists;
-            // 如果加载了online日志  则loadRedoLog为true
-            loadRedoLog = true;
         }
 
-        LOG.info(
-                "getEndScn success,startScn:{},endScn:{}, loadRedoLog:{}",
-                startScn,
-                endScn,
-                loadRedoLog);
-        return Pair.of(endScn, loadRedoLog);
+        LOG.info("getEndScn success,startScn:{},endScn:{}", startScn, endScn);
+        return endScn;
     }
 
     /** 获取logminer加载的日志文件 */
